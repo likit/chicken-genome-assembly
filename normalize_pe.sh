a5e5e4ed0f0b54b3dc6d527c8fce5bf650b1edd1
Likit Preeyanon <preeyano@msu.edu>
2014-01-03 15:01:06 -0500

    add normalize_pe.sh

---
+++ normalize_pe.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -login
+#PBS -l nodes=1:ppn=1,mem=48gb,walltime=12:00:00
+#PBS -M preeyano@msu.edu
+#PBS -m abe
+#PBS -N Digignorm_${PBS_JOBID}
+
+cd ${PBS_O_WORKDIR}
+
+if [ -z $loadtable ]
+then
+    ~/khmer/scripts/normalize-by-median.py -p -C 20 -k 20 -N 4 -x 1e10 --savehash ${savetable} ${input} -R ${input}.log
+else
+    ~/khmer/scripts/normalize-by-median.py -p -C 20 -k 20 -N 4 -x 1e10 --savehash ${savetable} --loadhash ${loadtable} ${input} -R ${input}.log
+fi
